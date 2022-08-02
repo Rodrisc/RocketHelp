44a00ff32030f12d0f2bb08e0240adc66f6d7ded
Rodrigo Soares <87383983+Rodrisc@users.noreply.github.com>
2022-08-02 12:50:59 -0300

    v1.0

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import React from 'react';
+import THEME  from './src/styles/theme';
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Routes } from './src/routes';
+
+export default function App() {
+
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
+    
+    
+  );
+}
+
+
